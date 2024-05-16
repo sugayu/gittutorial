59081ff3c2d859f773bffb4fdde00cf3c88fae50
sugayu <sugayu@aoni.waseda.jp>
2024-05-16 09:24:16 +0900

    First commit

---
+++ src/gittutorial/__init__.py
@@ -0,0 +1,8 @@
+'''Tutorial codes for Git.
+
+This is a simple explanation of your package,
+but you may not explan the details here
+because users will understand what the package does by reading README.
+'''
+
+from .module import print_favorites, print_what_working_on
---
+++ src/gittutorial/module.py
@@ -0,0 +1,29 @@
+'''Main module of gittutorial.
+
+Everyone who is joining in tutorial will edit this module.
+It is recommended to explain your module in the docstring at the top, here.
+'''
+
+__all__ = ['print_favorites', 'print_what_working_on']
+
+
+def print_favorites() -> None:
+    '''Print your favorite things.
+
+    Please explain in the docstring what this function is.
+    '''
+    # "list[str]" clarifies that "favorites" contains strings.
+    favorites: list[str] = ['Git']
+
+    print('Your favorite things:')
+    for fav in favorites:
+        print(f'- {fav}')
+
+
+def print_what_working_on() -> None:
+    '''Print what you are working on.'''
+    works: list[str] = ['JWST and ALMA observations of high redshift galaxies']
+
+    print('What are you working on?:')
+    for w in works:
+        print(f'- {w}')
